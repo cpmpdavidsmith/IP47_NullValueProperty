0d081817c2785e16c3deb99a0f3bc49b699fbdff
David Smith <cpmpdavidsmith@icloud.com>
2024-01-16 16:22:37 -0500

    prac

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace Nullable
+{
+    class Program
+    {
+        static void Main()
+        {
+            int? a = null;
+            Console.WriteLine(a.HasValue); //HasValue property is false
+
+            a = 6;//assign value to variable
+            Console.WriteLine(a.HasValue); //hasvalue property is true because a has non-null value
+            Console.WriteLine(a.Value);  //returns value of a
+            Console.WriteLine(a);
+            Console.Read();
+
+
+        }
+    }
+}
